99dd397be64aab2124cd68deda8cfdb42872bfe0
duyetdev <me@duyetdev.com>
2015-08-09 14:52:51 +0700

    Add support help, using -h

--- static-html-server.js
+++ static-html-server.js
@@ -10,6 +10,24 @@ var argv = mnm(process.argv.slice(2))
   , port = argv.p || 7788
   , root = argv.r || process.cwd()
   , fallbackPath = argv.f
+  , help = argv.h || false;
+
+if (argv.h) {
+  console.log("\nNODE STATIC SERVER");
+  console.log("   Simple static web server.\
+    \n   Author: Van-Duyet Le (me@duyetdev.com). \
+    \n   Website: http://duyetdev.com");
+  console.log("\nUsage:\
+    \n    static-html-server -p [port] -r [root folder] -f [fallback path if not found]");
+  console.log("\nArguments (all are optional):\
+    \n    - p: [Number] port number, default to 8000\
+    \n    - r: [String] root folder, default to working directory\
+    \n    - f: [String] fallback path when page not found, default to not falling back and send 404\n\n");
+  console.log("For example\
+    \n    $ static-html-server -p 8899 -r ./ -f index.html\
+    \n    Server running at http://localhost:8899/ [root: ./, fallback: index.html]\n");
+  process.exit(1);
+}
 
 if (fallbackPath) fallbackPath = path.join(root, fallbackPath)
 
